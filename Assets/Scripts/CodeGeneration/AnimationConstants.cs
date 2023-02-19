1a9af2fe799b2286c7115567f55fc1085fc24460
alimertcetin <mrtmert54@gmail.com>
2023-02-19 18:40:07 +0300

    Generated AnimationConstants

---
+++ Assets/Scripts/CodeGeneration/AnimationConstants.cs
@@ -0,0 +1,24 @@
+public static class AnimationConstants
+{
+	// Animation names
+	public const string AJ_Jumping = "Jumping";
+	public const string AJ_Neutral_Idle = "Neutral_Idle";
+	public const string AJ_Walking = "Walking";
+	public const string AJ_Running2 = "Running2";
+	public const string Keycard_Animation_Keycard = "Animation_Keycard";
+	public const string Number_Animation_Sayi = "Animation_Sayi";
+	public const string Door_LeftSide_Open = "LeftSide_Open";
+	public const string Door_LeftSide_Close = "LeftSide_Close";
+	public const string Door_RightSide_Open = "RightSide_Open";
+	public const string Door_RightSide_Close = "RightSide_Close";
+	// Animator parameters
+	public const string AJ_Jump_Trigger = "Jump";
+	public const string AJ_Speed_Float = "Speed";
+	public const string Door_LeftSide_Open_Bool = "LeftSide_Open";
+	public const string Door_LeftSide_Close_Bool = "LeftSide_Close";
+	public const string Door_RightSide_Open_Bool = "RightSide_Open";
+	public const string Door_RightSide_Close_Bool = "RightSide_Close";
+	public const string Door_AnimationSpeed_Float = "AnimationSpeed";
+	public const string TestController_DoTransitionToState1_Trigger = "DoTransitionToState1";
+	public const string TestController_DoTransitionToState2_Trigger = "DoTransitionToState2";
+}
