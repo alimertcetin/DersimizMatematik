de5022d304dfbd4c0eeb2daaf92ef2f61121607a
alimertcetin <mrtmert54@gmail.com>
2023-02-19 18:46:33 +0300

    GameUI.cs now uses tween

--- Assets/Scripts/UI/Temp/GameUI.cs
+++ Assets/Scripts/UI/Temp/GameUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using ElRaccoone.Tweens;
+using UnityEngine;
 
 namespace LessonIsMath.UI
 {
@@ -9,13 +10,28 @@ namespace LessonIsMath.UI
         protected virtual void Awake() => UISystem.AddUI(this);
         public virtual void Show()
         {
+            var scale = gameObject.transform.localScale;
+            uiGameObject.transform.localScale = Vector3.zero;
             uiGameObject.SetActive(true);
+            uiGameObject.TweenCancelAll();
+            uiGameObject.TweenLocalScale(scale, 0.5f)
+                .SetEaseElasticInOut()
+                .SetOnComplete(() => uiGameObject.GetComponent<CanvasGroup>().interactable = true);
             isActive = true;
         }
 
         public virtual void Hide()
         {
-            uiGameObject.SetActive(false);
+            uiGameObject.TweenCancelAll();
+            var scale = gameObject.transform.localScale;
+            uiGameObject.TweenLocalScale(Vector3.zero, 1f)
+                .SetEaseElasticInOut()
+                .SetOnComplete(() =>
+                {
+                    uiGameObject.GetComponent<CanvasGroup>().interactable = false;
+                    uiGameObject.SetActive(false);
+                    uiGameObject.transform.localScale = scale;
+                });
             isActive = false;
         }
 
