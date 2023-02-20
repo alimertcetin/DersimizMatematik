e87f1ef86e991233a529634416f059994f35fd0e
alimertcetin <mrtmert54@gmail.com>
2023-02-20 17:57:27 +0300

    Created TweenExtensions class
    Added a ClickTween method for button click

---
+++ Assets/Scripts/Tween/TweenExtensions.cs
@@ -0,0 +1,37 @@
+﻿using ElRaccoone.Tweens;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LessonIsMath.Tween
+{
+    public static class TweenExtensions
+    {
+        public static void ClickTween(this Button button, float duration)
+        {
+            const float tweenFactor = 0.9f;
+
+            button.TweenCancelAll();
+            var buttonTransform = button.transform;
+            var pos = buttonTransform.position;
+            var scale = buttonTransform.localScale;
+
+            var newPos = pos + Vector3.up * tweenFactor;
+            var newScale = scale * tweenFactor;
+            button.TweenPosition(newPos, duration)
+                .TweenLocalScale(newScale, duration)
+                .SetOnComplete(() =>
+                {
+                    button.TweenPosition(pos, duration)
+                    .TweenLocalScale(scale, duration).
+                    SetOnCancel(OnCancel);
+                })
+                .SetOnCancel(OnCancel);
+
+            void OnCancel()
+            {
+                buttonTransform.localScale = scale;
+                buttonTransform.position = pos;
+            }
+        }
+    }
+}
