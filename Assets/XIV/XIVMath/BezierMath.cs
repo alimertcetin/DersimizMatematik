0d35b322085532cba163f76836f074106cfb0aaf
alimertcetin <mrtmert54@gmail.com>
2023-02-28 11:59:59 +0300

    XIV.XIVMath.BezierMath - Added GetTime function

--- Assets/XIV/XIVMath/BezierMath.cs
+++ Assets/XIV/XIVMath/BezierMath.cs
@@ -7,6 +7,9 @@ namespace XIV.XIVMath
     /// </summary>
     public static class BezierMath
     {
+        const float TOLERANCE = 0.0001f;
+        const int GET_TIME_ITERATION_COUNT = 10;
+
         /// <summary>
         /// Returns the point at curve depending on <paramref name="t"/> time
         /// </summary>
@@ -27,6 +30,29 @@ namespace XIV.XIVMath
             t = Mathf.Clamp01(t);
             float oneMinusT = 1f - t;
             return 3f * oneMinusT * oneMinusT * (p1 - p0) + 6f * oneMinusT * t * (p2 - p1) + 3f * t * t * (p3 - p2);
+        }
+
+        public static float GetTime(Vector3 currentPosition, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float tolarence = TOLERANCE, int iteration = GET_TIME_ITERATION_COUNT)
+        {
+            float currentGuess = 0.5f; // initial guess for t
+            
+            for (int i = 0; i < iteration; i++)
+            {
+                Vector3 pointOnCurve = GetPoint(p0, p1, p2, p3, currentGuess);
+                Vector3 tangentAtPoint = GetFirstDerivative(p0, p1, p2, p3, currentGuess);
+                float distanceToTarget = Vector3.Distance(currentPosition, pointOnCurve);
+                float slopeOfDistance = Vector3.Dot(currentPosition - pointOnCurve, tangentAtPoint);
+
+                if (distanceToTarget < tolarence)
+                {
+                    break;
+                }
+
+                currentGuess += slopeOfDistance / tangentAtPoint.sqrMagnitude;
+                currentGuess = Mathf.Clamp01(currentGuess);
+            }
+
+            return currentGuess;
         }
     }
 }
