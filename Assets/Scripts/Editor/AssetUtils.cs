d5d34847b1f02d2371910d95e2bfb14d38286654
alimertcetin <mrtmert54@gmail.com>
2023-03-19 07:26:08 +0300

    Created AssetUtils

---
+++ Assets/Scripts/Editor/AssetUtils.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+namespace XIV.EditorUtils
+{
+    public static class AssetUtils
+    {
+        /// <summary>Load .asset files via their base class</summary>
+        /// <typeparam name="TAsset">Asset Type</typeparam>
+        /// <returns>Dictionary that contains the types of assets as key and value as the list of objects</returns>
+        public static Dictionary<Type, List<TAsset>> LoadAssets<TAsset>(string folderPath, SearchOption searchOption = SearchOption.AllDirectories) 
+            where TAsset : UnityEngine.Object
+        {
+            Dictionary<Type, List<TAsset>> typeValuePair = new Dictionary<Type, List<TAsset>>();
+            string[] assetPaths = Directory.GetFiles(folderPath, "*.asset", searchOption);
+                
+            for (int i = 0; i < assetPaths.Length; i++)
+            {
+                TAsset asset = AssetDatabase.LoadAssetAtPath<TAsset>(assetPaths[i]);
+                if (asset == null) continue;
+                            
+                var assetType = asset.GetType();
+                if (typeValuePair.ContainsKey(assetType))
+                {
+                    typeValuePair[assetType].Add(asset);
+                }
+                else
+                {
+                    List<TAsset> list = new List<TAsset>();
+                    list.Add(asset);
+                    typeValuePair.Add(assetType, list);
+                }
+            }
+
+            return typeValuePair;
+        }
+    }
+}
