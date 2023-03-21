b7464384ff2fb9caf29c7d522deb6401189c8b8d
alimertcetin <mrtmert54@gmail.com>
2023-03-21 13:12:57 +0300

    Created IListExtensions

---
+++ Assets/XIV/Extensions/IListExtensions.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace XIV.Extensions
+{
+    public static class IListExtensions
+    {
+        public static T PickRandom<T>(this IList<T> list)
+        {
+            return list[UnityEngine.Random.Range(0, list.Count)];
+        }
+    }
+}
