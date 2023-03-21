689e023f942f19e21f4b718cc15114bdd4bfccd9
alimertcetin <mrtmert54@gmail.com>
2023-03-21 14:49:27 +0300

    ComponentIListExtensions update

--- Assets/XIV/Extensions/ComponentIListExtensions.cs
+++ Assets/XIV/Extensions/ComponentIListExtensions.cs
@@ -31,29 +31,6 @@ namespace XIV.Extensions
         {
             return GetClosest(searchArray, currentPosition, out _);
         }
-        
-        public static T GetClosestOnXZPlane<T>(this IList<T> searchArray, Vector3 currentPosition) where T : Component
-        {
-            var length = searchArray.Count;
-            var distance = float.MaxValue;
-            if (length == 0) return default;
-
-            T selected = default;
-
-            currentPosition = currentPosition.OnXZ();
-            for (int i = 0; i < length; i++)
-            {
-                var current = searchArray[i];
-                var dis = Vector3.Distance(currentPosition, current.transform.position.OnXZ());
-                if (dis < distance)
-                {
-                    distance = dis;
-                    selected = current;
-                }
-            }
-            
-            return selected;
-        }
 
         public static T GetClosest<T>(this IList<T> searchArray, Vector3 currentPosition, out float distance, params T[] exclude) where T : Component
         {
@@ -80,6 +57,34 @@ namespace XIV.Extensions
         public static T GetClosest<T>(this IList<T> searchArray, Vector3 currentPosition, params T[] exclude) where T : Component
         {
             return GetClosest(searchArray, currentPosition, out _, exclude);
+        }
+        
+        public static T GetClosestOnXZPlane<T>(this IList<T> searchArray, Vector3 currentPosition, out float distance) where T : Component
+        {
+            var length = searchArray.Count;
+            distance = float.MaxValue;
+            if (length == 0) return default;
+
+            T selected = default;
+
+            currentPosition = currentPosition.OnXZ();
+            for (int i = 0; i < length; i++)
+            {
+                var current = searchArray[i];
+                var dis = Vector3.Distance(currentPosition, current.transform.position.OnXZ());
+                if (dis < distance)
+                {
+                    distance = dis;
+                    selected = current;
+                }
+            }
+            
+            return selected;
+        }
+        
+        public static T GetClosestOnXZPlane<T>(this IList<T> searchArray, Vector3 currentPosition) where T : Component
+        {
+            return GetClosestOnXZPlane(searchArray, currentPosition, out _);
         }
     }
 }
