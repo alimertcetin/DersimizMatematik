4ab51d3b64c2226dc3ed99853d40ca9fbf39cb61
alimertcetin <mrtmert54@gmail.com>
2023-03-22 14:16:25 +0300

    HudUI - didnt remove it instead changed it
    I know its not good...

--- Assets/Scripts/HUD_UI.cs
+++ Assets/Scripts/HUD_UI.cs
@@ -1,46 +1,140 @@
 ﻿using UnityEngine;
-using TMPro;
 using LessonIsMath.UI;
+using TMPro;
+using UnityEngine.UI;
+using XIV.EventSystem;
+using XIV.EventSystem.Events;
+using XIV.Extensions;
+using XIV.InventorySystem.UI;
 
 namespace XIV.UI
 {
     public class HUD_UI : GameUI
     {
-        // TODO : Remove this class
-    }
+        [SerializeField] InventoryPanel numbers;
+        [SerializeField] InventoryPanel keycards;
+        [SerializeField] float alphaFadeDuration = 0.5f;
+        Image[] uiImages;
+        TMP_Text[] uiTexts;
+        float[] uiImagesDefaultAlphas;
+        float[] uiTextsDefaultAlphas;
+        IEvent customTweenEvent;
 
-    public class HUDNumberUI : MonoBehaviour
-    {
-        [SerializeField] RectTransform contentParent;
-        [SerializeField] GameObject numberPrefab;
-
-        private void Awake()
+        protected override void Awake()
         {
-            for (int i = 0; i < 9; i++)
+            base.Awake();
+            uiImages = uiGameObject.GetComponentsInChildren<Image>();
+            uiTexts = uiGameObject.GetComponentsInChildren<TMP_Text>();
+
+            int imageLength = uiImages.Length;
+            uiImagesDefaultAlphas = new float[imageLength];
+            for (int i = 0; i < imageLength; i++)
             {
-                var numberGo = Object.Instantiate(numberPrefab, contentParent);
-                numberGo.GetComponent<NumberUIItem>().SetValue(i);
+                uiImagesDefaultAlphas[i] = uiImages[i].color.a;
+            }
+
+            int textsLength = uiTexts.Length;
+            uiTextsDefaultAlphas = new float[textsLength];
+            for (int i = 0; i < textsLength; i++)
+            {
+                uiTextsDefaultAlphas[i] = uiTexts[i].color.a;
             }
         }
-    }
 
-    public class NumberUIItem : MonoBehaviour
-    {
-        int value;
-        int amount;
-        public TMP_Text text;
-        public TMP_Text amountText;
+        public void ShowNumbers() => numbers.gameObject.SetActive(true);
+        public void HideNumbers() => numbers.gameObject.SetActive(false);
+        public void ShowKeycards() => keycards.gameObject.SetActive(true);
+        public void HideKeycards() => keycards.gameObject.SetActive(false);
 
-        public void SetValue(int value)
+        public override void Show()
         {
-            this.value = value;
-            text.text = value.ToString();
+            // really need a custom tween system...
+            XIVEventSystem.CancelEvent(customTweenEvent);
+            for (var i = 0; i < uiImages.Length; i++)
+            {
+                uiImages[i].gameObject.SetActive(true);
+            }
+            for (var i = 0; i < uiTexts.Length; i++)
+            {
+                uiTexts[i].gameObject.SetActive(true);
+            }
+
+            int imagesLength = uiImages.Length;
+            float[] imageStartColorA = new float[imagesLength];
+            for (int i = 0; i < imagesLength; i++)
+            {
+                imageStartColorA[i] = uiImages[i].color.a;
+            }
+
+            int textsLength = uiTexts.Length;
+            var textStartColorA = new float[textsLength];
+            for (int i = 0; i < textsLength; i++)
+            {
+                textStartColorA[i] = uiTexts[i].color.a;
+            }
+            customTweenEvent = new InvokeForSecondsEvent(alphaFadeDuration).AddAction((timer) =>
+                {
+                    for (var i = 0; i < uiImages.Length; i++)
+                    {
+                        Image image = uiImages[i];
+                        image.color = image.color.SetA(Mathf.Lerp(imageStartColorA[i], uiImagesDefaultAlphas[i], timer.NormalizedTime));
+                    }
+                    for (var i = 0; i < uiTexts.Length; i++)
+                    {
+                        var tmpText = uiTexts[i];
+                        tmpText.color = tmpText.color.SetA(Mathf.Lerp(textStartColorA[i], uiTextsDefaultAlphas[i], timer.NormalizedTime));
+                    }
+                })
+                .OnCompleted(() => isActive = true)
+                .OnCanceled(() => isActive = true);
+            XIVEventSystem.SendEvent(customTweenEvent);
         }
 
-        public void SetAmount(int value)
+        public override void Hide()
         {
-            this.value = value;
-            text.text = value.ToString();
+            XIVEventSystem.CancelEvent(customTweenEvent);
+
+            int imagesLength = uiImages.Length;
+            float[] imageStartColorA = new float[imagesLength];
+            for (int i = 0; i < imagesLength; i++)
+            {
+                imageStartColorA[i] = uiImages[i].color.a;
+            }
+
+            int textsLength = uiTexts.Length;
+            var textStartColorA = new float[textsLength];
+            for (int i = 0; i < textsLength; i++)
+            {
+                textStartColorA[i] = uiTexts[i].color.a;
+            }
+            customTweenEvent = new InvokeForSecondsEvent(alphaFadeDuration).AddAction((timer) =>
+                {
+                    
+                    for (var i = 0; i < uiImages.Length; i++)
+                    {
+                        Image image = uiImages[i];
+                        image.color = image.color.SetA(Mathf.Lerp(imageStartColorA[i], 0f, timer.NormalizedTime));
+                    }
+                    for (var i = 0; i < uiTexts.Length; i++)
+                    {
+                        var tmpText = uiTexts[i];
+                        tmpText.color = tmpText.color.SetA(Mathf.Lerp(textStartColorA[i], 0f, timer.NormalizedTime));
+                    }
+                })
+                .OnCompleted(() =>
+                {
+                    for (var i = 0; i < uiImages.Length; i++)
+                    {
+                        uiImages[i].gameObject.SetActive(false);
+                    }
+                    for (var i = 0; i < uiTexts.Length; i++)
+                    {
+                        uiTexts[i].gameObject.SetActive(false);
+                    }
+                    isActive = false;
+                })
+                .OnCanceled(() => isActive = false);
+            XIVEventSystem.SendEvent(customTweenEvent);
         }
     }
 }
