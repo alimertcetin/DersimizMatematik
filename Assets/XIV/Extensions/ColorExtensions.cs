22d99be67210d6942a61554149cd85a193f5a085
alimertcetin <mrtmert54@gmail.com>
2023-03-22 13:55:43 +0300

    Created ColorExtensions

---
+++ Assets/XIV/Extensions/ColorExtensions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace XIV.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color SetR(this Color color, float value)
+        {
+            color.r = value;
+            return color;
+        }
+        
+        public static Color SetG(this Color color, float value)
+        {
+            color.g = value;
+            return color;
+        }
+        
+        public static Color SetB(this Color color, float value)
+        {
+            color.b = value;
+            return color;
+        }
+        
+        public static Color SetA(this Color color, float value)
+        {
+            color.a = value;
+            return color;
+        }
+    }
+}
