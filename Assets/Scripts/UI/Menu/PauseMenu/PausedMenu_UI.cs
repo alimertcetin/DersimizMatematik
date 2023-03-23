2b6ad88edda4aff1e182af68ed8ccd30d62a6e46
alimertcetin <mrtmert54@gmail.com>
2023-03-24 00:23:04 +0300

    PauseMenu_UI update

--- Assets/Scripts/UI/Menu/PauseMenu/PausedMenu_UI.cs
+++ Assets/Scripts/UI/Menu/PauseMenu/PausedMenu_UI.cs
@@ -1,45 +1,105 @@
 ﻿using LessonIsMath.Input;
 using LessonIsMath.ScriptableObjects.ChannelSOs;
+using LessonIsMath.UI.Components;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using XIV.SaveSystems;
 
 namespace LessonIsMath.UI
 {
-    public class PausedMenu_UI : GameUI
+    public class PausedMenu_UI : GameUI, PlayerControls.IGameUIActions
     {
+        [SerializeField] BoolEventChannelSO pauseMenuUIChannel;
         [Header("Broadcasting To")]
-        [SerializeField] private StringEventChannelSO WarningUIChannel = default;
-        [SerializeField] private LoadEventChannelSO onExitPressed = default;
+        [SerializeField] StringEventChannelSO warningUIChannel;
+        [SerializeField] LoadEventChannelSO locationLoadChannel;
 
         [Header("Scene To Unload")]
         [Tooltip("If exit pressed, unload this scene")]
-        [SerializeField] private GameSceneSO gamePlayScene = default;
+        [SerializeField] GameSceneSO gamePlayScene;
 
         [Header("Scenes To Load")]
-        [SerializeField] private GameSceneSO mainMenu = default;
+        [SerializeField] GameSceneSO mainMenu;
 
-        public void btn_Load()
+        [SerializeField] GameObject mainPage;
+        [SerializeField] SettingsUIPage settingsUIPage;
+        [SerializeField] CustomButton btn_Resume;
+        [SerializeField] CustomButton btn_Load;
+        [SerializeField] CustomButton btn_Save;
+        [SerializeField] CustomButton btn_Settings;
+        [SerializeField] CustomButton btn_Exit;
+
+        void OnEnable()
+        {
+            btn_Resume.RegisterOnClick(Resume);
+            btn_Save.RegisterOnClick(Save);
+            btn_Load.RegisterOnClick(Load);
+            btn_Settings.RegisterOnClick(ShowSettings);
+            btn_Exit.RegisterOnClick(OnExit);
+        }
+
+        void OnDisable()
+        {
+            btn_Resume.UnregisterOnClick();
+            btn_Save.UnregisterOnClick();
+            btn_Load.UnregisterOnClick();
+            btn_Settings.UnregisterOnClick();
+            btn_Exit.UnregisterOnClick();
+        }
+
+        public override void Show()
+        {
+            InputManager.GameState.Disable();
+            InputManager.GameUI.SetCallbacks(this);
+            InputManager.GameUI.Enable();
+            InputManager.Interaction.Disable();
+            base.Show();
+            mainPage.SetActive(true);
+        }
+
+        public override void Hide()
+        {
+            InputManager.GameState.Enable();
+            InputManager.GameUI.Disable();
+            InputManager.Interaction.Enable();
+            base.Hide();
+        }
+
+        void Resume()
+        {
+            pauseMenuUIChannel.RaiseEvent(false);
+        }
+
+        void Save()
+        {
+            SaveSystem.Save();
+            warningUIChannel.RaiseEvent("Saved : " + Application.persistentDataPath);
+        }
+
+        void Load()
         {
             SaveSystem.Load();
         }
 
-        public void btn_Save()
+        void ShowSettings()
         {
-            SaveSystem.Save();
-            WarningUIChannel.RaiseEvent("Kayıt Edildi : " + Application.persistentDataPath);
+            InputManager.GameUI.Disable();
+            settingsUIPage.Show();
+            mainPage.SetActive(false);
         }
 
-        public void btn_Settings()
+        void OnExit()
         {
-            UISystem.Show<Settings_UI>();
-        }
-
-        public void btn_Exit()
-        {
-            onExitPressed.RaiseEvent(mainMenu, true);
+            locationLoadChannel.RaiseEvent(mainMenu, true);
             InputManager.DisableAllInput();
             gamePlayScene.sceneReference.UnLoadScene();
             gameObject.SetActive(false);
+        }
+
+        void PlayerControls.IGameUIActions.OnExit(InputAction.CallbackContext context)
+        {
+            if (context.performed == false) return;
+            Resume();
         }
     }
 }
