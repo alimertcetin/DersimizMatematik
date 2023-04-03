07d6b3b1d6662de8d3124e815c115c6f05072bcd
alimertcetin <mrtmert54@gmail.com>
2023-04-03 13:18:57 +0300

    SaveSystem update

--- Assets/Scripts/SaveSystems/SaveSystem.cs
+++ Assets/Scripts/SaveSystems/SaveSystem.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 namespace XIV.SaveSystems
 {
     public static class SaveSystem
     {
-        private static string SavePath => Path.Combine(Application.persistentDataPath, "14.Filo");
+        static string CurrentScene => SceneManager.GetActiveScene().name;
+        static string saveDirectory => Path.Combine(Application.persistentDataPath, CurrentScene);
+        static string saveFile => CurrentScene + ".sav";
+        static string SavePath => Path.Combine(saveDirectory, saveFile);
 
         public static void Save()
         {
@@ -22,20 +27,34 @@ namespace XIV.SaveSystems
             RestoreState(state);
         }
 
-        private static void SaveFile(object state)
+        public static IEnumerator SaveAsync()
         {
-            using (var stream = File.Open(SavePath, FileMode.Create))
+            var state = LoadFile();
+            yield return CaptureStateAsync(state);
+            SaveFile(state);
+        }
+
+        public static IEnumerator LoadAsync()
+        {
+            var state = LoadFile();
+            yield return RestoreStateAsync(state);
+        }
+
+        static void SaveFile(object state)
+        {
+            if (Directory.Exists(saveDirectory) == false) Directory.CreateDirectory(saveDirectory);
+            using (var stream = File.Open(SavePath, FileMode.Create, FileAccess.Write))
             {
                 var formatter = new BinaryFormatter();
                 formatter.Serialize(stream, state);
             }
         }
 
-        private static Dictionary<string, object> LoadFile()
+        static Dictionary<string, object> LoadFile()
         {
             if (File.Exists(SavePath) == false) return new Dictionary<string, object>();
 
-            using (FileStream stream = File.Open(SavePath, FileMode.Open))
+            using (FileStream stream = File.Open(SavePath, FileMode.Open, FileAccess.Read))
             {
                 var formatter = new BinaryFormatter();
                 return (Dictionary<string, object>)formatter.Deserialize(stream);
@@ -43,7 +62,7 @@ namespace XIV.SaveSystems
         }
 
 
-        private static void CaptureState(Dictionary<string, object> state)
+        static void CaptureState(Dictionary<string, object> state)
         {
             var saveables = Object.FindObjectsOfType<SaveableEntity>();
             foreach (var saveable in saveables)
@@ -55,11 +74,38 @@ namespace XIV.SaveSystems
             }
         }
 
-        private static void RestoreState(Dictionary<string, object> state)
+
+        static IEnumerator CaptureStateAsync(Dictionary<string, object> state)
         {
             var saveables = Object.FindObjectsOfType<SaveableEntity>();
             foreach (var saveable in saveables)
             {
+                yield return null;
+                var saveableEntityState = saveable.CaptureState();
+
+                if (state.ContainsKey(saveable.Id)) state[saveable.Id] = saveableEntityState;
+                else state.Add(saveable.Id, saveableEntityState);
+            }
+        }
+
+        static void RestoreState(Dictionary<string, object> state)
+        {
+            var saveables = Object.FindObjectsOfType<SaveableEntity>();
+            foreach (var saveable in saveables)
+            {
+                if (state.TryGetValue(saveable.Id, out object value))
+                {
+                    saveable.RestoreState(value);
+                }
+            }
+        }
+
+        static IEnumerator RestoreStateAsync(Dictionary<string, object> state)
+        {
+            var saveables = Object.FindObjectsOfType<SaveableEntity>();
+            foreach (var saveable in saveables)
+            {
+                yield return null;
                 if (state.TryGetValue(saveable.Id, out object value))
                 {
                     saveable.RestoreState(value);
