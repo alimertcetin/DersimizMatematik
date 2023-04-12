c7888997d76bfa38ad1edf299c2f176dbff8d00c
alimertcetin <mrtmert54@gmail.com>
2023-04-12 22:58:34 +0300

    ArrayExtensions update

--- Assets/XIV/Extensions/ArrayExtensions.cs
+++ Assets/XIV/Extensions/ArrayExtensions.cs
@@ -50,5 +50,28 @@ namespace XIV.Extensions
             }
             return count;
         }
+
+        public static T[] RemoveAt<T>(this T[] arr, int index)
+        {
+            int length = arr.Length;
+            if (index < 0 || index > length) return arr;
+            for (int i = index; i < length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            if (length - 1 >= 0) Array.Resize(ref arr, length - 1);
+            return arr;
+        }
+
+        public static T[] RemoveIf<T>(this T[] arr, Func<T, bool> condition)
+        {
+            int length = arr.Length;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (condition.Invoke(arr[i])) arr = RemoveAt(arr, i);
+            }
+
+            return arr;
+        }
     }
 }
